f6989cdd6102ca1d4a8df85b49d6072bae24561e
bertrand <bertrand.maubert.info@gmail.com>
2022-06-11 22:49:45 +0200

    On a ajouté un sprite. Le sprite se déplace.

--- game.py
+++ game.py
@@ -2,6 +2,9 @@ import pygame
 import pyscroll
 import pytmx
 
+from player import Player
+
+
 class Game:
 
     def __init__(self):
@@ -14,18 +17,39 @@ class Game:
         tmx_data = pytmx.util_pygame.load_pygame('carte.tmx')
         map_data = pyscroll.data.TiledMapData(tmx_data)
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
-        map_layer.zoom = 2
-        # Dessiner le group de calc
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
+        map_layer.zoom = 1
+       # generer un joueur
+        player_position = tmx_data.get_object_by_name('player')
+        self.player = Player(player_position.x, player_position.y)
+
+        # Dessiner le groupe de calc
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=0)
+        self.group.add(self.player)
+
+    def handle_input(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP]:
+            print("UP")
+            self.player.move_up()
+        elif pressed[pygame.K_DOWN]:
+            self.player.move_down()
+        elif pressed[pygame.K_LEFT]:
+            self.player.move_left()
+        elif pressed[pygame.K_RIGHT]:
+            self.player.move_right()
+
     def run(self):
+        clock = pygame.time.Clock()
 
         running = True
-
         while running:
+            self.handle_input()
+            self.group.update()
+            self.group.center(self.player.rect.center)
             self.group.draw(self.screen)
             pygame.display.flip()
             for even in pygame.event.get():
                 if even.type == pygame.QUIT:
                     running = False
-
+            clock.tick(60) # nb images /sec
         pygame.quit()
---
+++ player.py
@@ -0,0 +1,30 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, x, y ):
+        super().__init__()
+        self.sprite_sheet = pygame.image.load('player.png')
+        self.image = self.get_image(0, 0)
+        self.image.set_colorkey([0, 0, 0])
+        self.rect = self.image.get_rect() # position du sprite
+        self.position = [x, y]
+
+    def move_right(self):
+
+        self.position[0] += 3
+
+    def move_left(self):
+        self.position[0] -= 3
+
+    def move_up(self): self.position[1] -= 3
+
+    def move_down(self): self.position[1] += 3
+
+    def update(self):
+        self.rect.topleft = self.position
+
+    def get_image(self, x, y):
+        image = pygame.Surface([32,32])
+        image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
+        return image
