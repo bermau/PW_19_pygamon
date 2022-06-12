498f147fdf7b18565dfdeb55e58e48aa4f085e0e
bertrand <bertrand.maubert.info@gmail.com>
2022-06-12 08:59:29 +0200

    Détection des collisions

--- game.py
+++ game.py
@@ -26,25 +26,43 @@ class Game:
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=0)
         self.group.add(self.player)
 
+        # Définir une liste de collisions
+        self.walls = []
+        for obj in tmx_data.objects:
+            if obj.type == "collision":
+                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+
     def handle_input(self):
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_UP]:
-            print("UP")
             self.player.move_up()
+            self.player.change_animation('up')
         elif pressed[pygame.K_DOWN]:
             self.player.move_down()
+            self.player.change_animation('down')
         elif pressed[pygame.K_LEFT]:
             self.player.move_left()
+            self.player.change_animation('left')
         elif pressed[pygame.K_RIGHT]:
             self.player.move_right()
+            self.player.change_animation('right')
+
+    def update(self):
+        self.group.update()
+        # verif coollision
+        for sprite in self.group.sprites():
+            if sprite.feet.collidelist(self.walls) > -1:
+                sprite.move_back()
+
 
     def run(self):
         clock = pygame.time.Clock()
 
         running = True
         while running:
+            self.player.save_location()
             self.handle_input()
-            self.group.update()
+            self.update()
             self.group.center(self.player.rect.center)
             self.group.draw(self.screen)
             pygame.display.flip()
--- player.py
+++ player.py
@@ -10,19 +10,41 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect() # position du sprite
         self.position = [x, y]
 
-    def move_right(self):
+        self.images = {
+            'down' : self.get_image(0, 0),
+            'left' : self.get_image(0, 32),
+            'right' : self.get_image(0, 64),
+            'up' : self.get_image(0, 96)
+        }
+        self.feet = pygame.Rect(0, 0, self.rect.width * 0.5 , 12)
+        self.old_position = self.position.copy()
+        self.speed = 2
 
-        self.position[0] += 3
+    def save_location(self):
+        self.old_position = self.position.copy()
+
+    def change_animation(self, name):
+        self.image = self.images[name]
+        self.image.set_colorkey((0, 0, 0))
+
+    def move_right(self):
+        self.position[0] += self.speed
 
     def move_left(self):
-        self.position[0] -= 3
+        self.position[0] -= self.speed
 
-    def move_up(self): self.position[1] -= 3
+    def move_up(self): self.position[1] -= self.speed
 
-    def move_down(self): self.position[1] += 3
+    def move_down(self): self.position[1] += self.speed
 
     def update(self):
         self.rect.topleft = self.position
+        self.feet.midbottom = self.rect.midbottom
+
+    def move_back(self):
+        self.position = self.old_position
+        self.rect.topleft = self.position
+        self.feet.midbottom = self.rect.midbottom
 
     def get_image(self, x, y):
         image = pygame.Surface([32,32])
