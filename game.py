b036473b215b490e7588418636b740ea7670004e
bertrand <bertrand.maubert.info@gmail.com>
2022-06-14 00:38:38 +0200

    OK : Le personnage entre et sort de la maison.

--- game.py
+++ game.py
@@ -9,11 +9,12 @@ class Game:
 
     def __init__(self):
         pygame.init()
-        # créer venêtre du jeu
+        # créer la fenêtre du jeu
 
         self.screen = pygame.display.set_mode((800,600))
         pygame.display.set_caption("Dinosaure aventure")
         # Charger les cartes
+        self.map = 'world'
         tmx_data = pytmx.util_pygame.load_pygame('carte.tmx')
         map_data = pyscroll.data.TiledMapData(tmx_data)
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
@@ -22,18 +23,24 @@ class Game:
         player_position = tmx_data.get_object_by_name('player')
         self.player = Player(player_position.x, player_position.y)
 
-        # Dessiner le groupe de calc
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=0)
-        self.group.add(self.player)
-
         # Définir une liste de collisions
         self.walls = []
         for obj in tmx_data.objects:
             if obj.type == "collision":
                 self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
 
+        # Dessiner le groupe de calc
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=8)
+        self.group.add(self.player)
+
+        # rect de collision pour entrer dans la maison
+        enter_house = tmx_data.get_object_by_name('enter_house')
+        self.enter_house_rect = pygame.Rect(enter_house.x, enter_house.y, enter_house.width, enter_house.height)
+
+
     def handle_input(self):
         pressed = pygame.key.get_pressed()
+
         if pressed[pygame.K_UP]:
             self.player.move_up()
             self.player.change_animation('up')
@@ -47,12 +54,79 @@ class Game:
             self.player.move_right()
             self.player.change_animation('right')
 
+    def switch_house(self):
+        print("Switch to house")
+        # Charger les cartes
+        tmx_data = pytmx.util_pygame.load_pygame('house.tmx')
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        map_layer.zoom = 2
+
+        # Définir une liste de collisions
+        self.walls = []
+
+        for obj in tmx_data.objects:
+            if obj.type == "collision":
+                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+
+        # Dessiner le groupe des calques
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=8)
+        self.group.add(self.player)
+
+        # rect de collision pour aller dans le jardin
+        enter_world = tmx_data.get_object_by_name('enter_world')
+        self.enter_world_rect = pygame.Rect(enter_world.x, enter_world.y, enter_world.width, enter_world.height)
+
+        # Gestion du spawn dans la maison
+        spawn_house = tmx_data.get_object_by_name('spawn_house')
+        self.player.position[0] = spawn_house.x
+        self.player.position[1] = spawn_house.y - 50
+
+    def switch_world(self):
+        self.map = 'world'
+        print("Switch to world")
+        # Charger les cartes
+        tmx_data = pytmx.util_pygame.load_pygame('carte.tmx')
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        map_layer.zoom = 1
+
+        # Définir une liste de collisions
+        self.walls = []
+
+        for obj in tmx_data.objects:
+            if obj.type == "collision":
+                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+
+        # Dessiner le groupe de calques
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=8)
+        self.group.add(self.player)
+
+        # rect de collision pour aller dans la maison
+        enter_house = tmx_data.get_object_by_name('enter_house')
+        self.enter_house_rect = pygame.Rect(enter_house.x, enter_house.y, enter_house.width, enter_house.height)
+
+        # récupèrer le point de spawn dans le jardin
+        spawn_house_point = tmx_data.get_object_by_name('spawn_world')
+        self.player.position[0] = spawn_house_point.x
+        self.player.position[1] = spawn_house_point.y - 30
+
     def update(self):
         self.group.update()
-        # verif coollision
+        # verif collision
         for sprite in self.group.sprites():
             if sprite.feet.collidelist(self.walls) > -1:
                 sprite.move_back()
+
+        # Entrée sortie de la maison
+        if self.map == 'world' and self.player.feet.colliderect(self.enter_house_rect):
+            self.switch_house()
+            self.map = 'house'
+
+        # Entrée dans le jardin
+        if self.map == 'house' and self.player.feet.colliderect(self.enter_world_rect):
+            self.switch_world()
+            self.map = 'world'
 
 
     def run(self):
