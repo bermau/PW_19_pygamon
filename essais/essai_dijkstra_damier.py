e768f2383e78fc17ef81291f5a885f6097d773f9
bertrand <bertrand.maubert.info@gmail.com>
2023-02-20 21:48:30 +0100

    OK ++ réussit à parcourir tout le plateau. Trouve une solution correcte !

--- essais/essai_dijkstra_damier.py
+++ essais/essai_dijkstra_damier.py
@@ -1,73 +1,77 @@
 # Un test de l'algorithme de Dijkstra
 # Adaptation à un damier
 
-import sys
-from pprint import pprint
+from random import randint
 
 def title(msg):
-    long = len (msg) + 4
+    long = len(msg) + 4
     print("*" * long)
     print(f"* {msg} *")
     print("*" * long)
 
 
 class Point:
-    def __init__(self, x, y ):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def __str__(self):
+        return f"Point ({self.x}, {self.y})"
 
     def __repr__(self):
         return f"Point ({self.x}, {self.y})"
 
     def top(self):
-        return Point(self.x-1, self.y)
+        return Point(self.x - 1, self.y)
 
     def bottom(self):
         return Point(self.x + 1, self.y)
 
     def left(self):
-        return Point(self.x, self.y-1)
+        return Point(self.x, self.y - 1)
 
     def right(self):
-        return Point(self.x, self.y+1)
+        return Point(self.x, self.y + 1)
+
 
 MAX = 9999
 
-class Graph():
 
-    def __init__(self, x, y ):
+class Graph:
+
+    def __init__(self, x, y):
         self.row_nb = x
         self.col_nb = y
-        self.graph = [[0 for _ in range(y)] for row in range(x)]
-        self.distance = [[MAX for _ in range(y)] for row in range(x)]
-        self.visited = [[False for _ in range(y)] for row in range(x)]
-        self.parent = [[None for _ in range(y)] for row in range(x)]
+        self.graph = [[0 for _ in range(y)] for _ in range(x)]
+        self.distance = [[MAX for _ in range(y)] for _ in range(x)]
+        self.visited = [[False for _ in range(y)] for _ in range(x)]
+        self.parent = [[None for _ in range(y)] for _ in range(x)]
 
     def print_visited(self):
         print(' . 0 1 2 3 4 5 6 7 8')
         for i, row in enumerate(self.visited):
-            print("{:>2}".format(str(i)), end='') #  "{:>2}"
+            print("{:>2}".format(str(i)), end='')  # "{:>2}"
             for r in row:
-                if r == True:
-                    print (' X', end = '')
+                if r:
+                    print(' X', end='')
                 else:
-                    print("  ", end = '')
+                    print("  ", end='')
             print()
 
     def print_distances(self):
         print('  .  0  1  2  3  4  5  6  7  8')
         for i, row in enumerate(self.distance):
-            print("{:>3}".format(str(i)), end='') #  "{:>2}"
+            print("{:>3}".format(str(i)), end='')  # "{:>2}"
             for r in row:
-                if r == MAX :
-                    print ('  /', end = '')
+                if r == MAX:
+                    print('  /', end='')
                 else:
                     print("{:>3}".format(str(r)), end='')
             print()
 
     def pSol(self):
-        print("Distance of node from source : ")
-        print(self.distance)
+        print("**** oSol ****")
+        self.print_distances()
         print("Parent of last move :")
         print(self.parent)
 
@@ -79,7 +83,7 @@ class Graph():
         if self.parent[node] == source_node:
             print("Et vous êtes arrivé.")
         else:
-            self.describe(source_node, self.parent[node] )
+            self.describe(source_node, self.parent[node])
 
     def describe(self, source_node, dest_node):
         """Return the shortest path between 2 nodes"""
@@ -93,9 +97,9 @@ class Graph():
         path.reverse()
         print(path)
         for point in path:
-            print(f"({point})", end = '')
+            print(f"({point})", end='')
             if point != dest_node:
-                print(f" -- {self.graph[point][path[point+1]]} -> ", end='')
+                print(f" -- {self.graph[point][path[point + 1]]} -> ", end='')
             else:
                 print(" Cout total : ", self.distance[dest_node])
         return path
@@ -109,60 +113,74 @@ class Graph():
         print('Point en entrée de sélection', p)
         neighbors = [[p, '0'], [p.top(), 'T'], [p.bottom(), 'B'], [p.left(), 'L'], [p.right(), 'R']]
         for v, dir in neighbors:
-                if self.distance[v.x][v.y] < local_min and not self.visited[v.x][v.y]:
-                    local_min = self.distance[v.x][v.y]
-                    best_index = v
-                    best_direction = dir
+            if self.distance[v.x][v.y] < local_min and not self.visited[v.x][v.y]:
+                local_min = self.distance[v.x][v.y]
+                best_index = v
+                best_direction = dir
 
         return best_index, best_direction
 
+    def choose_non_visited_rnd_point(self):
+        while True:
+            p = Point(randint(1, self.row_nb -2), randint(1, self.col_nb-2))
+            if not self.visited[p.x][p.y]:
+                break
+        return p
+
+
     def dijkstra(self, source):
-        """Calculates all shortest distances from source to any node"""
+        """Calculates all shortest distances from source to all nodes."""
         self.distance[source.x][source.y] = 0
         self.visited[source.x][source.y] = False
-
         u = source
+        loop_i = 0
+        while True:
+            loop_i += 1
+            for passage in range(20):
+                self.print_visited()
+                self.print_distances()
+                title("Passage " + str(passage))
+                u, dir = self.node_with_min_distance(u) # During first execution, return the source point.
+                if not u:
+                    break
+                print("Noeud non visité avec la plus petite distance : ", u, ". On travaille maintenant sur ce noeud.")
+                self.visited[u.x][u.y] = True
+                print(f"Le noeud {u} est marqué comme visité.")
+                self.print_visited()
+                # print(f"ligne pour u = {u} vaut : {self.graph[u]}")
+                # On examine ici tous les mouvements possibles depuis v. Soit 4 points
+                neighbors = [[u.top(), 'T'], [u.bottom(), 'B'], [u.left(), 'L'], [u.right(), 'R']]
+                for neighbor, dir in neighbors:
+                    print(f"Noeud neighbor {neighbor} , val = {self.graph[neighbor.x][neighbor.y]} .", end='')
+                    if not self.visited[neighbor.x][neighbor.y]:
+                        if self.graph[neighbor.x][neighbor.y] == 0:
+                            print(f"Valeur possible ?\nEst-ce que cela vaut le coup d'aller de {u} à {neighbor} ?",
+                                  f"Alors que nous avons déjà une solution pour aller en {neighbor} par une autre voie ? ")
+                            print(f"Le cout actuel pour {neighbor} : {self.distance[neighbor.x][neighbor.y]}",
+                                  f"Cout actuel pour arriver sur {u} : {self.distance[u.x][u.y]}, "
+                                  f"Pour passer de {u} à {neighbor} : 1 ", end='')
 
-        for passage in range(20):
-
-            self.print_visited()
-            self.print_distances()
-
-            title("Passage " + str(passage))
-            u, dir = self.node_with_min_distance(u)
-            print("Noeud non visité avec la plus petite distance : ", u, ". On travaille maintenant sur ce noeud.")
-
-            self.visited[u.x][u.y] = True
-            print(f"Le noeud {u} est marqué comme visité.")
-            self.print_visited()
-
-            # print(f"ligne pour u = {u} vaut : {self.graph[u]}")
-            # On examine ici tous les mouvements possibles depuis v. Soit 4 points
-            neighbors = [[u.top(), 'T'], [u.bottom(), 'B'], [u.left(), 'L'], [u.right(), 'R']]
-            for neighbor, dir in neighbors:
-                print(f"Noeud neighbor {neighbor} , val = {self.graph[neighbor.x][neighbor.y]} .", end = '')
-                if not self.visited[neighbor.x][neighbor.y]:
-                    if self.graph[neighbor.x][neighbor.y] == 0:
-                        print(f"Valeur possible ?\nEst-ce que cela vaut le coup d'aller de {u} à {neighbor} ?",
-                              f"Alors que nous avons déjà une solution pour aller en {neighbor} par une autre voie ? ")
-                        print(f"Le cout actuel pour {neighbor} : {self.distance[neighbor.x][neighbor.y]}",
-                              f"Cout actuel pour arriver sur {u} : {self.distance[u.x][u.y]}, "
-                              f"Pour passer de {u} à {neighbor} : {self.graph[u.x][u.y]} ", end = '')
-
-                        if self.distance[neighbor.x][neighbor.y] > self.distance[u.x][u.y] + self.graph[neighbor.x][neighbor.y]:
-                            print(f"Valeur retenue ! {self.distance[u.x][u.y]  + self.graph[neighbor.x][neighbor.y]}")
-                            self.distance[neighbor.x][neighbor.y] = self.distance[u.x][u.y]+ self.graph[neighbor.x][neighbor.y]
-                            self.parent[neighbor.x][neighbor.y]= dir
+                            if self.distance[neighbor.x][neighbor.y] > self.distance[u.x][u.y] + 1:
+                                print(f"Valeur retenue ! {self.distance[u.x][u.y] + 1}")
+                                self.distance[neighbor.x][neighbor.y] = self.distance[u.x][u.y] + 1
+                                self.parent[neighbor.x][neighbor.y] = dir
+                            else:
+                                print("Trajet plus coûteux donc non intéressant")
                         else:
-                            print("Trajet non intéressant")
+                            print("Mur")
+                            self.visited[neighbor.x][neighbor.y] = True
                     else:
-                        print("Mur")
-                        self.visited[neighbor.x][neighbor.y] = True
+                        print("déjà visité")
+                print("All neighbors visited")
+                self.print_distances()
+            self.pSol()
 
-                else:
-                    print("déjà visité")
-            self.print_distances()
-        self.pSol()
+            # On passe à un autre point aléatoire
+            u = self.choose_non_visited_rnd_point()
+            print()
+            title("NOUVEAU ALEATOIR " + str(loop_i))
+            print ("Nouveau point aléatoire : " , u)
+        print(f"Tout semble exploré après {loop_i} passages.")
 
 
 f = Graph(6, 9)
