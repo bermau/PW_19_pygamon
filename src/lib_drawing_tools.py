4e0ed869a3b89eea539648fb73cb6ce9b31913e6
bertrand <bertrand.maubert.info@gmail.com>
2023-03-12 11:13:48 +0100

    New : Ajout d'une grille

--- src/lib_drawing_tools.py
+++ src/lib_drawing_tools.py
@@ -4,14 +4,31 @@ import pygame
 
 display = True
 
+
 class DebugRect():
     def __init__(self, color, pygame_rect, l_width):
-
         self.zone = pygame_rect
         self.color = color
         self.l_width = l_width
 
     def render(self, screen):
-
         if display:
             pygame.draw.rect(screen, self.color, self.zone, self.l_width)
+
+
+
+def render_world_grill(screen, map):
+    reduc = 2
+    tw = map.tmx_data.tilewidth * reduc
+    th = map.tmx_data.tileheight * reduc
+    w = map.tmx_data.width
+    h = map.tmx_data.height
+
+    for x in range(0, w * tw, tw):
+        for y in range(0, h * th, th):
+            rect = pygame.Rect(x, y, tw, th)
+            pygame.draw.rect(screen, "white", rect, 1)
+
+def render_simple_world(screen, map):
+
+    pass
