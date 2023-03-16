34652499263047c5bd0cff4e64e87df37dcf1a60
bertrand <bertrand.maubert.info@gmail.com>
2023-03-16 11:07:59 +0100

    2 NPC affichés

--- src/map.py
+++ src/map.py
@@ -120,8 +120,8 @@ class MapManager:
         self.register_map('world',
                           portals=[Portal(from_world="world", origin_point='enter_house', target_world="house",
                                           teleport_point="spawn_from_world")],
-                          # npcs=[  NPC('paul', self, 'world'),
-                          #      NPC('robin', self, 'world')],
+                          npcs=[  NPC('paul', self, 'world'),
+                               NPC('robin', self, 'world')],
                           )
 
         # Ajouter un rectangle indicateur dans la carte world.
