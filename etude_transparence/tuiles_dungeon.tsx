3bdf0edecc7e67634194e2af9ff536f6ebf5d322
bertrand <bertrand.maubert.info@gmail.com>
2025-11-17 23:01:20 +0100

    création d'une carte d'essai dite monde.tmx

---
+++ etude_transparence/tuiles_dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="tuiles_dungeon" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="ts_dungeon.png" trans="ffffff" width="256" height="256"/>
+</tileset>
