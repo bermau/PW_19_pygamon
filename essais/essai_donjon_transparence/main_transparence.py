72f6d4652f1127dc27ce4af7d9ccf4f131f34566
bertrand <bertrand.maubert.info@gmail.com>
2025-11-29 09:31:45 +0100

    transparence méthode 1

--- essais/essai_donjon_transparence/main_transparence.py
+++ essais/essai_donjon_transparence/main_transparence.py
@@ -1,6 +1,5 @@
 # Importing the library
 from time import sleep
-
 import pygame
 
 # Initializing Pygame
@@ -9,24 +8,26 @@ pygame.init()
 # Initializing surface
 fenetre = pygame.display.set_mode((400, 300))
 
-# Insérer un tmx
+# color :
+fenetre.fill("pink")
 
-# Nom du fichier TMX (même dossier)
-TMX_FILE = "ts_dungeon.png"
+# Create a blue surface
+blue_surface = pygame.Surface((50, 50))
+blue_surface.fill("blue")
 
-# Créer un surface bleue
-surf_2_bleue = pygame.Surface((50,50))
-surf_2_bleue.fill("red")
-# placer la surface bleue
-fenetre.blit(surf_2_bleue, (100,20))
-# Initialing Color
-color_rect = (155, 120, 0)
+fenetre.blit(blue_surface, (100, 20))
 
+# Create a red surface
+red_surface = pygame.Surface((100, 60))
+red_surface.set_alpha(100)
+red_surface.fill((155, 120, 0))
+
+
+fenetre.blit(red_surface,(30, 30))
 
 # Drawing Rectangle
-pygame.draw.rect(fenetre, color_rect, pygame.Rect(30, 30, 60, 60))
+# pygame.draw.rect(fenetre, red_surface, pygame.Rect(30, 30, 100, 60))
 
+# display for 2 seconds
 pygame.display.flip()
-
 sleep(2)
-# input("OK3")
