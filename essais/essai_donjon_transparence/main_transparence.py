8db28cc3763b6f02aab4103301f674b82a36a5b5
bertrand <bertrand.maubert.info@gmail.com>
2025-11-29 10:38:10 +0100

    transparence méthode 2 et chargement d'une image avec font transparent.

--- essais/essai_donjon_transparence/main_transparence.py
+++ essais/essai_donjon_transparence/main_transparence.py
@@ -6,9 +6,7 @@ import pygame
 pygame.init()
 
 # Initializing surface
-fenetre = pygame.display.set_mode((400, 300))
-
-# color :
+fenetre = pygame.display.set_mode((600, 400))
 fenetre.fill("pink")
 
 # Create a blue surface
@@ -17,17 +15,18 @@ blue_surface.fill("blue")
 
 fenetre.blit(blue_surface, (100, 20))
 
-# Create a red surface
-red_surface = pygame.Surface((100, 60))
-red_surface.set_alpha(100)
-red_surface.fill((155, 120, 0))
-
-
+# Create a red surface with alpha
+red_surface = pygame.Surface((100, 60), pygame.SRCALPHA)
+# red_surface.set_alpha(100)
+red_surface.fill((155, 120, 0, 100))
 fenetre.blit(red_surface,(30, 30))
 
-# Drawing Rectangle
-# pygame.draw.rect(fenetre, red_surface, pygame.Rect(30, 30, 100, 60))
+# import a png (which has a transparent layout)
+file = "ts_dungeon.png"
+png_surface = pygame.image.load(file)
+png_surface.set_alpha(50)
+fenetre.blit(png_surface, (200,100))
 
 # display for 2 seconds
 pygame.display.flip()
-sleep(2)
+sleep(5)
